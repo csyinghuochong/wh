71a774d7a38586e138201ac8b344df7a3799dd0b
DESKTOP-QQOLK7G\LDYQ <394266315@qq.com>
2026-08-07 14:03:24 +0800

    no message

--- Model/Danger/JiaYuan/JiaYuanComponentServer.cs
+++ Model/Danger/JiaYuan/JiaYuanComponentServer.cs
@@ -17,9 +17,6 @@ namespace ET
     public class JiaYuanComponentServer : Entity, IAwake, IDestroy, ITransfer, IDeserialize, IUnitCache
     {
 
-
-
-
         public long RefreshMonsterTime_2 = 0;
 
         public long JiaYuanDaShiTime_1 = 0;
